e88208ba8bc2fdd2154e67570013471377a055b3
linyuchu <yuchu.lin@my.jcu.edu.au>
2025-10-03 14:22:11 +0800

    Add password check program

---
+++ password_stars.py
@@ -0,0 +1,12 @@
+# Setting minimum length of password
+MIN_LENGTH = 6
+
+# Keep asking for input until the conditions are met
+while True:
+    password = input("Enter password :".format(MIN_LENGTH))
+    if len(password) >= MIN_LENGTH:
+        break
+
+# Output asterisks with the same length as the password
+print("*" * len(password))
+
